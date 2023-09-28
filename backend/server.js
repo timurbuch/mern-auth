f12d0ccb421bc1c2f12e82cf3f9a16e22d30eb43
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-28 15:20:27 +0200

    Connect mongodb Database

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-const port = process.env.PORT || 5000
-import userRoutes from './routes/userRoutes.js'
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+const port = process.env.PORT || 5000;
+import userRoutes from './routes/userRoutes.js';
 
-const app = express()
+connectDB();
 
-app.use('/api/users/', userRoutes)
+const app = express();
 
-app.get('/', (req, res) => res.send('Server is ready'))
+app.use('/api/users/', userRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.get('/', (req, res) => res.send('Server is ready'));
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
