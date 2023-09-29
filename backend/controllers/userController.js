1137b1dbc2acf9d93f749f14c957af08043a623f
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-29 06:40:04 +0200

    Register User endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,38 +1,70 @@
-import asyncHandler from 'express-async-handler'
-
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 //@desc Auth user/set token
 //route POST api/users/auth
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Auth User'})
-}) 
+  res.status(200).json({ message: 'Auth User' });
+});
 
 //@desc Register a new user
 //route POST api/users
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Register User'})
-}) 
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+
+  res.status(200).json({ message: 'Register User' });
+});
 
 //@desc Logout user
 //route POST api/users/logout
 //@access Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Logout User'})
-}) 
+  res.status(200).json({ message: 'Logout User' });
+});
 
 //@desc Get user profile
 //route GET api/users/profile
 //@access Private
-const getUserProfile= asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'User Profile'})
-}) 
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User Profile' });
+});
 
 //@desc Update user profile
 //route PUT api/users/profile
 //@access Private
-const updateUserProfile= asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Update user Profile'})
-}) 
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Update user Profile' });
+});
 
-export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
