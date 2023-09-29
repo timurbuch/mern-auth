5941b00080fe6effaedf6b3f67693c0acedd3d87
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-29 18:37:26 +0200

    Add middleware to get and update routes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,9 +12,12 @@ const protect = asyncHandler(async (req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.userId).select('-password');
+
+      next()
     } catch (error) {
       res.status(401);
       throw new Error('Not authorized, invalid token');
+      
     }
   } else {
     res.status(401);
