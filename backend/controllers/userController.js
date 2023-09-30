109e2b38ac907ecd7a999bda4036c604627b4690
Timur Buchberger <timo.buchberger@gmail.com>
2023-09-30 15:43:55 +0200

    Update user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,8 +20,6 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(401);
     throw new Error('Invalid email or password');
   }
-
-  res.status(200).json({ message: 'Auth User' });
 });
 
 //@desc Register a new user
@@ -62,30 +60,47 @@ const registerUser = asyncHandler(async (req, res) => {
 //route POST api/users/logout
 //@access Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.cookie('jwt', '', {
-        httpOnly: true,
-        expires: new Date(0)
-    })
-  res.status(200).json({ message: 'User logged out' });
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: 'Logged out successfully' });
 });
 
 //@desc Get user profile
 //route GET api/users/profile
 //@access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    const user = {
-        _id: req.user._id,
-        name: req.user.name,
-        email: req.user.email,
-    }
-    res.status(200).json(user);
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 //@desc Update user profile
 //route PUT api/users/profile
 //@access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Update user Profile' });
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
 });
 
 export {
